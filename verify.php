93f15bc68b85f2f21a8fc63e3aff230ae689620e
Isard BOTHA <ibotha@e4r15p6.wethinkcode.co.za>
2018-10-24 16:57:28 +0200

    confirm

--- verify.php
+++ verify.php
@@ -1,6 +1,16 @@
 <?php
 	require 'config/setup.php';
-	$sql = "SELECT * FROM `users` WHERE email = :email OR username = :username LIMIT 1";
+	$sql = "SELECT * FROM `users` WHERE verif = :verif LIMIT 1";
 	$users_req = $conn->prepare($sql);
-	$_GET['key'];
+	$users_req->bindParam(":verif", $_GET['key']);
+	$users_req->execute();
+	$row = $users_req->fetch(PDO::FETCH_ASSOC);
+	$activate = "UPDATE users SET `active` = 1 WHERE verif = :verif LIMIT 1;";
+	$act = $conn->prepare($activate);
+	$act->bindParam(":verif", $_GET['key']);
+	if ($row['active'] == 0)
+		$act->execute();
+	if ($row['active'] == 1)
+		echo $row['username'];
+		
 ?>
